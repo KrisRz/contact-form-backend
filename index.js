729f977573e2e80624ef98e9c93e8cb2b0ecf9fe
KrisRz <163850783+KrisRz@users.noreply.github.com>
2025-02-08 14:52:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = express();
 
 // ✅ Allow CORS for local development (127.0.0.1:5500) and live website
 const corsOptions = {
-  origin: ["https://krisgrzepka.uk", "http://localhost:5500"], // Allow both local and deployed frontends
+  origin: ["https://krisgrzepka.uk", "http://localhost:5500", "http://127.0.0.1:5500"],
   methods: "POST",
   allowedHeaders: ["Content-Type"],
 };
 
-app.use(cors(corsOptions));
+app.use(cors()); // Allow all origins temporarily  --  app.use(cors(corsOptions));
 app.use(express.json());
 
 // ✅ Setup API Route for sending emails
